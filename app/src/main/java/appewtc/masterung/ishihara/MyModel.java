feda3a3e1c3dbd592c6e8f8338b2ee22467368fa
masterUNG <phrombutr@gmail.com>
2015-06-09 13:56:36 +0700

    Create Model

---
+++ app/src/main/java/appewtc/masterung/ishihara/MyModel.java
@@ -0,0 +1,18 @@
+package appewtc.masterung.ishihara;
+
+/**
+ * Created by masterUNG on 6/9/15 AD.
+ */
+public class MyModel {
+
+    private int modelAnInt;
+
+    public int getModelAnInt() {
+        return modelAnInt;
+    }   // getter
+
+    public void setModelAnInt(int modelAnInt) {
+        this.modelAnInt = modelAnInt;
+    }   // setter
+    
+}   // Main Class
